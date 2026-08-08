3c3956f221560c2d2ad6cdf15ff2bcf588ff31ec
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-08-09 01:15:59 +0500

    Time: 0 ms (100%), Space: 8.1 MB (84.25%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string res;
+        for(int i=0;i<s.size();i++){
+          res+=tolower(s[i]); 
+           
+        }
+        return res;
+    } 
+};
