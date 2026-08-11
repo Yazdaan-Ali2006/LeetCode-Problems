73a567b167e8a58359a9c8a7c439bc02c3e36b00
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-08-11 15:50:04 +0500

    Time: 0 ms (100%), Space: 8.4 MB (83.76%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int gcd(int a, int b)
+{
+ 
+
+    while (1)
+    {   int rem = a % b;
+        if (rem == 0)
+        {
+            return b;
+        }
+        a = b;
+        b = rem;
+       
+    }
+
+}
+    int gcdOfOddEvenSums(int n) {
+    int sumeven = n * (n + 1);
+    int sumodd = n * n;
+    return gcd(sumodd, sumeven);
+}
+};
