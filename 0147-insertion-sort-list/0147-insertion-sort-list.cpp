2611d463b2e0967ab8ed9d0c126a89bbd8e6753d
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-08-21 23:39:29 +0500

    Time: 23 ms (54.42%), Space: 14.7 MB (54.34%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            
+            ListNode* next = curr->next;
+            
+            ListNode* temp = dummy;
+
+            while (temp->next != NULL && temp->next->val < curr->val) {
+                temp = temp->next;
+            }
+
+            curr->next = temp->next;
+            temp->next = curr;
+
+            curr = next;
+        }
+
+        return dummy->next;
+    }
+};
