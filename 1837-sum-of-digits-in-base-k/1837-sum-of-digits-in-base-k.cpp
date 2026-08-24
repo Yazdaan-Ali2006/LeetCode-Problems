056a5b7eed72ccf6f65ba9642d7bd8e5d6f4c424
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-08-24 22:28:50 +0500

    Time: 0 ms (100%), Space: 7.8 MB (47.51%) - LeetHub

---
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int rem = 0;
+        while (n != 0) {
+            rem += n % k;
+            n /= k;
+        }
+        return rem;
+    }
+};
