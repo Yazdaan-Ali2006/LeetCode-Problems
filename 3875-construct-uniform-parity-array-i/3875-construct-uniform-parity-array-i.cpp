a6e8541e3a60de7e4d4c1345585dac28d40d914d
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-09-02 16:05:43 +0500

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+      //if all of them are even or odd=true
+      //if it is the case of even +odd then by subtraction i9 can make the whole parity same so true here aswell
+        
         return true;
         
     }
