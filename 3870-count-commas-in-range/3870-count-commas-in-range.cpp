f98559064e5b779845f4ce6bac03a8577a10fdff
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-09-08 17:28:07 +0500

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if (n < 1000) {
+            return 0;
+        }
+        int count = 0;
+            return count += n - 999;
+        
+    }
+};
