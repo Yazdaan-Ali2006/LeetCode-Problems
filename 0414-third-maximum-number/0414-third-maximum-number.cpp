55a2eadaf5ab9f99c5b1e5631ce412217763756e
Yazdaan Ali Mirza <yazdaancreation@gmail.com>
2026-09-17 21:06:08 +0500

    Time: 0 ms (100%), Space: 13 MB (46.9%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        // nums.erase(unique(nums.begin(), nums.end()), nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                for(int j=i+1;j<nums.size()-1;j++){
-                    nums[j]=nums[j+1];
-                }
-                nums.pop_back();
-                i--;
-                //for the sake of checking same element;
-            }
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         for(int j=i+1;j<nums.size()-1;j++){
+        //             nums[j]=nums[j+1];
+        //         }
+        //         nums.pop_back();
+        //         i--;
+        //         //for the sake of checking same element;
+        //     }
             
-        }
+        // }
         if (nums.size() >= 3)
         {
             return nums[(nums.size() - 1) - 2];
